30cee509189fedf14c4b45463108fa35eca07de8
Kostya-x-pro <Kostya-voronkov1992@rambler.ru>
2024-07-10 10:41:17 +0300

    Добавил удаление персонажа из базы данных по эталонный вариант (без коментариев Ивана)

--- src/components/heroesList/HeroesList.js
+++ src/components/heroesList/HeroesList.js
@@ -1,8 +1,13 @@
 import {useHttp} from '../../hooks/http.hook';
-import { useEffect } from 'react';
+import { useEffect, useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { heroesFetching, heroesFetched, heroesFetchingError } from '../../actions';
+import { 
+    heroesFetching, 
+    heroesFetched, 
+    heroesFetchingError,
+    heroDeleted,
+    } from '../../actions';
 import HeroesListItem from "../heroesListItem/HeroesListItem";
 import Spinner from '../spinner/Spinner';
 
@@ -25,6 +30,13 @@ const HeroesList = () => {
         // eslint-disable-next-line
     }, []);
 
+    const onDelete = useCallback((id) => {
+        request(`http://localhost:3001/heroes/${id}`, "DELETE")
+            .then(data => console.log(data))
+            .then(dispatch(heroDeleted(id)))
+            .catch(err => console.log(err))
+    }, [request])
+
     if (heroesLoadingStatus === "loading") {
         return <Spinner/>;
     } else if (heroesLoadingStatus === "error") {
@@ -37,7 +49,12 @@ const HeroesList = () => {
         }
 
         return arr.map(({id, ...props}) => {
-            return <HeroesListItem key={id} {...props}/>
+            return <HeroesListItem 
+                key={id}
+                {...props}
+                onDelete={() => onDelete(id)}
+
+            />
         })
     }
 
