bef8d2b52f155e086d036499d55949e6a5e3e1c3
Kostya-x-pro <Kostya-voronkov1992@rambler.ru>
2024-07-11 15:01:34 +0300

    Реализовам механизм усиления "store" который называется "enhancer" (который меняет поведение функции dispatch) для того что бы если в наш dispatch поподёт строка то всё будет работать нормально

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,28 @@
 import { createStore, combineReducers } from 'redux';
 import heroes from '../reducers/heroes';
 import filters from '../reducers/filters';
+
+// enhanser - это функция которая возвращает другую функцию и является усилителем "Store"
+const enhancer = (createStore) => (...args) => {
+  const store = createStore(...args);
+
+  const oldDispatch = store.dispatch;
+  store.dispatch = (action) => {
+    if (typeof action === 'string') {
+      return oldDispatch({
+        type: action
+      })
+    }
+    return oldDispatch(action)
+  }
+  
+  return store;
+}
+
 // В функцию combinerReducers нужно передовать объект (ключ и значение которых наши разбитые на кусочки части функции reducer (heroes и filters в дилнной и короткой записи))
-const store = createStore( combineReducers({heroes:heroes, filters}),
-window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = createStore( combineReducers({heroes:heroes, filters}), enhancer);
 
 export default store;
+
+
+// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
