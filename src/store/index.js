dec5e88a3354cbb4e8ced5b72c212a77de893d24
Kostya-x-pro <Kostya-voronkov1992@rambler.ru>
2024-07-11 11:09:14 +0300

    добавлен более оптимизированный вариант фильрации персонажей в самом компоненте "HeroesList" и удалены ненужные методы в функции "reducer". так же сам "reducer" был разделен на 2-е части что бы он не разростался и в "store" собран в одну функцию при помощи функции "combineReducer"

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,8 @@
-import { createStore } from 'redux';
-import reducer from '../reducers';
-
-const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+import { createStore, combineReducers } from 'redux';
+import heroes from '../reducers/heroes';
+import filters from '../reducers/filters';
+// В функцию combinerReducers нужно передовать объект (ключ и значение которых наши разбитые на кусочки части функции reducer (heroes и filters в дилнной и короткой записи))
+const store = createStore( combineReducers({heroes:heroes, filters}),
+window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 export default store;
