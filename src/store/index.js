0cc23f8de26f0d2bf4b1e5c9c95298a6f4d80246
Kostya-x-pro <Kostya-voronkov1992@rambler.ru>
2024-07-11 18:15:44 +0300

    добавлен в проект Redux-toolkit который позволяет намного проще создовать store и внём уже встроены многие конструкции (такие как combineReducers, compose, applyMiddleware. различные middleWare и прочее)

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
-import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
-import {thunk as ReduxThunk} from 'redux-thunk';
+// import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
+// import {thunk as ReduxThunk} from 'redux-thunk';
 import heroes from '../reducers/heroes';
 import filters from '../reducers/filters';
 
@@ -13,14 +14,32 @@ const stringMiddlewara = (store) => (next) => (action) => {
   return next(action)
 };
 
+// Создание store (с помощью Redux-toolkit)
+const store = configureStore({
+  // создаём наш разделённый на части редюсер
+  reducer: {
+    heroes,
+    filters
+  },
+  // подключение всех встроенных middleware + наш собственный 
+  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(stringMiddlewara),
+  // включаем наш redux-devtools (если просто передадим true то наш девтулс будет работать и на этапе bilda поэтому лучше использовать конструкцию которая показана ниже!)
+  devTools: process.env.NODE_ENV !== 'production',
+
+  // Подключаем наш middleware (reduxThunk и наш самописный) НО! в RTK есть встроенные middleware!!! (который мы подключаем выше)
+  // middleware: [ReduxThunk, stringMiddlewara],
+})
+
+
 // В функцию combinerReducers нужно передовать объект (ключ и значение которых наши разбитые на кусочки части функции reducer (heroes и filters в дилнной и короткой записи))
 // Функция "compose" помогает сделать композицию функций (скомбинировать несколько функций в одну) 
 // !!!В этой функции  важен порядок подключения enhancer-ов (если поменять enhancer и длинную строку window.__REDUX..... то работать не будет!!!)
-const store = createStore( 
-  combineReducers({heroes:heroes, filters}),
-  compose(
-      applyMiddleware(ReduxThunk, stringMiddlewara), 
-      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()) 
-);
+// const store = createStore( 
+//   combineReducers({heroes:heroes, filters}),
+//   compose(
+//       applyMiddleware(ReduxThunk, stringMiddlewara), 
+//       window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()) 
+// );
+
 
 export default store;
