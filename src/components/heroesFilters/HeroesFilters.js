38d218d216f6daee4743d79c4d2255346325b2e1
Kostya-x-pro <Kostya-voronkov1992@rambler.ru>
2024-07-13 11:13:26 +0300

    создал срез срез для фильтров и нашел ошибку (нужно передавать начальный данные из среза в начальный стейт).

--- src/components/heroesFilters/HeroesFilters.js
+++ src/components/heroesFilters/HeroesFilters.js
@@ -3,7 +3,8 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import classNames from 'classnames';
 
-import { fetchFilterHeroes, activeFilterChanged } from '../../actions';
+import { fetchFilterHeroes} from '../../actions';
+import { activeFilterChanged } from './filtersSlice';
 import Spinner from '../spinner/Spinner';
 
 const HeroesFilters = () => {
